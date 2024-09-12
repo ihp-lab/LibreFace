25cbeef07bd1b3c892575e72bfcb573340e1c31d
Ashutosh Chaubey <ashutoshchaubey2999@gmail.com>
2024-09-11 18:03:55 -0700

    Made changes to inference.py of AU_Detection to use the params from the code itself and not the YAML file.

--- libreface/AU_Detection/inference.py
+++ libreface/AU_Detection/inference.py
@@ -28,11 +28,37 @@ def set_seed(seed):
 def detect_action_units(image_path, model_path, device="cuda"):
     
     # Path to the YAML config file
-    config_path = './libreface/AU_Detection/config_au_detection.yaml'
+    # config_path = './libreface/AU_Detection/config_au_detection.yaml'
 
     # Load the configuration from YAML
-    config = load_config(config_path)
-    opts = ConfigObject(config)
+    # config = load_config(config_path)
+    opts = ConfigObject({'seed': 0,
+                        'data_root': '/home/ICT2000/dchang/TAC_project/data',
+                        'ckpt_path': './libreface/AU_Detection/fm_distillation_all/BP4D/all/resnet.pt',
+                        'data': 'BP4D',
+                        'fold': 'all',
+                        'num_workers': 0,
+                        'image_size': 256,
+                        'crop_size': 224,
+                        'num_labels': 12,
+                        'sigma': 10.0,
+                        'model_name': 'resnet',
+                        'dropout': 0.1,
+                        'hidden_dim': 128,
+                        'half_precision': False,
+                        'num_epochs': 30,
+                        'interval': 500,
+                        'threshold': 0.0,
+                        'batch_size': 256,
+                        'learning_rate': '3e-5',
+                        'weight_decay': '1e-4',
+                        'loss': 'unweighted',
+                        'clip': 1.0,
+                        'when': 10,
+                        'patience': 5,
+                        'fm_distillation': False,
+                        'device': 'cuda'
+                    })
 
     #set seed
     set_seed(opts.seed)
